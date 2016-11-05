0121c25f4171d51a561b0abce2c5bc5557684ce6
Tetiana Khomko <arnelli.tt@gmail.com>
2016-11-05 12:07:05 +0000

    first commit

---
+++ js/index.js
@@ -0,0 +1,36 @@
+//alert('Hello!');
+//console.log('Hello');
+//document.write('Hello');
+
+// var sum = 1974+3+8;
+// document.write('sum');
+// var text = 'My magic number';
+// var result = text + sum;
+// document.write(result);
+
+// var sum = 1974+3+8;
+// //document.right('sum');
+// var text = 'My magic number';
+// var result = text + sum;
+// document.write(result);
+
+
+
+// function sumArgs (a, b) {
+// var result = a = b;
+// return result;
+// }
+// function sumArgs (a, b, c) {
+// var result = a + b + c;
+// return result;
+    
+
+
+function tellAge('age') {
+    if (age < 18) {
+        return 'You are kid'; }
+        else {
+            return 'You are adult';
+        }
+    }
+}
